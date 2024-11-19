7ce3fcdab841ab7d254885953d5b610a13ae9082
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-11-19 11:58:11 +0700

    Tích hợp thư viện ShadCn UI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
 
 export default function Home() {
   return (
@@ -12,7 +14,8 @@ export default function Home() {
             </li>
             <li>Save and see your changes instantly.</li>
           </ol>
-
+          <Button variant={'outline'}>Học Next.js</Button>
+          <Input type='email' placeholder='Email' />
           <div className='flex gap-4 items-center flex-col sm:flex-row'>
             <a
               className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
