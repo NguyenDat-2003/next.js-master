a98af90a9aa9d8e9d062694e0b657a687b414264
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-11-20 13:56:12 +0700

    Xây dựng backend với TypeScript Fastify Prisma

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from '~/components/theme-provider'
+
+const inter = Inter({ subsets: ['vietnamese'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className='antialiased`'>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import ModeToggle from '~/components/ModeToggle'
 import { Button } from '~/components/ui/button'
 import { Input } from '~/components/ui/input'
 
@@ -14,6 +15,8 @@ export default function Home() {
             </li>
             <li>Save and see your changes instantly.</li>
           </ol>
+
+          <ModeToggle />
           <Button variant={'outline'}>Học Next.js</Button>
           <Input type='email' placeholder='Email' />
           <div className='flex gap-4 items-center flex-col sm:flex-row'>
@@ -36,35 +39,6 @@ export default function Home() {
             </a>
           </div>
         </main>
-        <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
-          <a
-            className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-            href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image aria-hidden src='/file.svg' alt='File icon' width={16} height={16} />
-            Learn
-          </a>
-          <a
-            className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-            href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image aria-hidden src='/window.svg' alt='Window icon' width={16} height={16} />
-            Examples
-          </a>
-          <a
-            className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-            href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image aria-hidden src='/globe.svg' alt='Globe icon' width={16} height={16} />
-            Go to nextjs.org →
-          </a>
-        </footer>
       </div>
     </>
   )
